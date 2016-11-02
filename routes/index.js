3f95610f3f289af9cb5379e91b18fc6c86461a38
Rhys <rhys.edwards91@gmail.com>
2016-11-02 22:54:28 +0000

    Now with POST Requests

--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,44 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+const $ = require('cheerio')
+const request = require('request-promise')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-let flightRequest = {
+const options = {
+  method: 'POST',
+  uri: 'https://www.googleapis.com/qpxExpress/v1/trips/search?&key=AIzaSyBp8BcIBr8IHKgi65Fw-CR06AxbnrwOp0I',
+  body: {
   "request": {
     "passengers": {
-      "kind": "qpxexpress#passengerCounts",
-      "adultCount": 1,
+      "adultCount": "1"
     },
     "slice": [
       {
-        "kind": "qpxexpress#sliceInput",
-        "origin": "LHR",
-        "destination": "OSL",
-        "date": "2016-12-03",
-        "permittedDepartureTime": {
-          "kind": "qpxexpress#timeOfDayRange",
-          "earliestTime": "06:00",
-          "latestTime": "11:00"
-        },
+        "origin": "SFO",
+        "destination": "LAX",
+        "date": "2017-06-19"
       }
     ],
-    "saleCountry": "GB",
-    "ticketingCountry": "GB",
-    "solutions": 10
+    "solutions": "5"
   }
+},
+  json: true
 }
 
-flightRequest.toString()
-console.log(flightRequest)
+request(options)
+  .then(function (response) {
+    console.log(response.trips.tripOption[0].saleTotal)
+    console.log(response.trips.tripOption[1].saleTotal)
+    console.log(response.trips.tripOption[2].saleTotal)
+    console.log(response.trips.tripOption[3].saleTotal)
+    console.log(response.trips.tripOption[4].saleTotal)
+  })
+  .catch(function (err) {
+    console.log(err)
+  })
 
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' })
+})
 
-module.exports = router;
+module.exports = router
