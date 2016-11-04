3c6783884b6cda4c79a469f7b5bc4ca68f2cb74b
Rhys Edwards <RhysEdwards@Rhyss-MacBook-Pro-2.local>
2016-11-04 13:26:17 +0000

    Data now renders in the view

--- api.js
+++ api.js
@@ -3,47 +3,49 @@ var router = express.Router()
 const $ = require('cheerio')
 const request = require('request-promise')
 
-var apiCall = function (cb) {
-    const options = {
-      method: 'POST',
-      uri: 'https://www.googleapis.com/qpxExpress/v1/trips/search?&key=AIzaSyBp8BcIBr8IHKgi65Fw-CR06AxbnrwOp0I',
-      body: {
-      "request": {
-        "passengers": {
-          "adultCount": "1"
-        },
-        "slice": [
-          {
-            "origin": "SFO",
-            "destination": "LAX",
-            "date": "2017-06-19"
-          }
-        ],
-        "solutions": "3"
-      }
-    },
-      json: true
+var apiCaller = function (cb) {
+  const options = {
+    method: 'POST',
+    uri: 'https://www.googleapis.com/qpxExpress/v1/trips/search?&key=AIzaSyBp8BcIBr8IHKgi65Fw-CR06AxbnrwOp0I',
+    body: {
+    "request": {
+      "passengers": {
+        "adultCount": "1"
+      },
+      "slice": [
+        {
+          "origin": "SFO",
+          "destination": "LAX",
+          "date": "2017-06-19"
+        }
+      ],
+      "solutions": "3"
     }
-
-    request(options)
-      .then(function (response) {
-        cb(response)
-      })
-      .catch(function (err) {
-        console.log(err)
-      })
+  },
+    json: true
   }
 
-  // Call the api with a call back
-  var apiGet = function(cb) {
-    return apiCaller(cb);
-  };
+  request(options)
+    .then(function (response) {
+      //console.log(response.trips.tripOption[0])
+      cb(response)
+    })
+    .catch(function (err) {
+      console.log(err)
+    })
+}
 
-  var apiPost = function(post, cb) {
-    return apiCaller(cb);
-  };
-  // Export the functions for external access
-  module.exports = {
-    apiGet: apiGet,
-    apiPost: apiPost
-  };
+// Call the api with a call back
+var apiGet = function(cb) {
+  return apiCaller(cb);
+};
+
+var apiPost = function(post, cb) {
+  return apiCaller(cb);
+};
+
+// Export the functions for external access
+module.exports = {
+  apiGet: apiGet,
+  apiPost: apiPost
+};
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 var express = require('express')
 var router = express.Router()
-var api = require('api')
+var api = require('../api')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   api.apiGet(function (data) {
-    res.render('index', {result: data})
+    console.log(data.trips.tripOption[0].slice[0].segment[0].cabin)
+    res.render('index', {
+      tripOption: data.trips.tripOption[0].slice[0].segment[0].cabin
+    })
   })
 })
 
