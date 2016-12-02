64e5891c856423b67960f44a9b723e8a7570a1bc
Rhys Edwards <rhys.edwards91@gmail.com>
2016-12-02 15:48:16 +0000

    Working to make autocomplete data external

---
+++ public/javascripts/autocomplete.v2.js
@@ -0,0 +1,37 @@
+$(function() {
+  $.ajax({
+    url: "/javascripts/airports.json",
+    dataType: "json",
+    success: function(request, response) {
+             var data = $.grep(suggestion, function(value) {
+               return value.city.substring(0, request.term.length).toLowerCase() == request.term.toLowerCase();
+             });
+             console.log('hello')
+            response(data)
+    }
+  })
+  $('#autocomplete').autocomplete({
+  minLength: 1,
+  source: data,
+  focus: function(event, ui) {
+              $('#autocomplete').val(ui.item.city,ui.item.country);
+              return false;
+          },
+
+  select: function(event, ui) {
+
+      $('#autocomplete').val(ui.item.name);
+
+              return false;
+          }
+      }).data( "ui-autocomplete" )._renderItem = function( ul, item ) {
+
+              return $( "<li></li>" )
+                  .data( "ui-autocomplete-item", item )
+
+                  .append( "<a>" + item.city + "," + item.country + "</a>" )
+
+                  .appendTo( ul );
+          };
+
+    });
